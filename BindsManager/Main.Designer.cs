f064187f53ae727e7bc1c505212ea5075d964786
Ecliptedkillz <65789295+Ecliptedkillz@users.noreply.github.com>
2022-08-31 16:06:06 -0400

    Add files via upload

---
+++ BindsManager/Main.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace BindsManager
+{
+	partial class Main
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.autoFillButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.addNewBindButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.commandTextBox = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.exportButton = new System.Windows.Forms.Button();
+            this.importButton = new System.Windows.Forms.Button();
+            this.openButton = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.helpbutton = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // autoFillButton
+            // 
+            this.autoFillButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.autoFillButton.Location = new System.Drawing.Point(12, 414);
+            this.autoFillButton.Name = "autoFillButton";
+            this.autoFillButton.Size = new System.Drawing.Size(245, 70);
+            this.autoFillButton.TabIndex = 0;
+            this.autoFillButton.Text = "Auto Fill for \r\nDuck Central Servers";
+            this.autoFillButton.UseVisualStyleBackColor = true;
+            this.autoFillButton.Click += new System.EventHandler(this.autoFillButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveButton.Location = new System.Drawing.Point(407, 392);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(167, 81);
+            this.saveButton.TabIndex = 1;
+            this.saveButton.Text = "Save";
+            this.saveButton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // addNewBindButton
+            // 
+            this.addNewBindButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addNewBindButton.Location = new System.Drawing.Point(663, 437);
+            this.addNewBindButton.Name = "addNewBindButton";
+            this.addNewBindButton.Size = new System.Drawing.Size(143, 29);
+            this.addNewBindButton.TabIndex = 2;
+            this.addNewBindButton.Text = "Add New Bind";
+            this.addNewBindButton.UseVisualStyleBackColor = true;
+            this.addNewBindButton.Click += new System.EventHandler(this.addNewBindButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(437, 340);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(113, 41);
+            this.clearButton.TabIndex = 3;
+            this.clearButton.Text = "Clear All";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(442, 308);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // commandTextBox
+            // 
+            this.commandTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.commandTextBox.Location = new System.Drawing.Point(663, 392);
+            this.commandTextBox.Name = "commandTextBox";
+            this.commandTextBox.Size = new System.Drawing.Size(184, 26);
+            this.commandTextBox.TabIndex = 6;
+            this.commandTextBox.Text = "Command Here";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(663, 358);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(143, 28);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.Text = "Select key";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(595, 361);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Key";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(574, 395);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Command";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.Location = new System.Drawing.Point(470, 12);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(451, 308);
+            this.richTextBox2.TabIndex = 13;
+            this.richTextBox2.Text = "";
+            // 
+            // exportButton
+            // 
+            this.exportButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exportButton.Location = new System.Drawing.Point(93, 326);
+            this.exportButton.Name = "exportButton";
+            this.exportButton.Size = new System.Drawing.Size(75, 23);
+            this.exportButton.TabIndex = 14;
+            this.exportButton.Text = "Save";
+            this.exportButton.UseVisualStyleBackColor = true;
+            this.exportButton.Click += new System.EventHandler(this.exportButton_Click);
+            // 
+            // importButton
+            // 
+            this.importButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.importButton.Location = new System.Drawing.Point(12, 326);
+            this.importButton.Name = "importButton";
+            this.importButton.Size = new System.Drawing.Size(75, 23);
+            this.importButton.TabIndex = 14;
+            this.importButton.Text = "Open";
+            this.importButton.UseVisualStyleBackColor = true;
+            this.importButton.Click += new System.EventHandler(this.importButton_Click);
+            // 
+            // openButton
+            // 
+            this.openButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.openButton.Location = new System.Drawing.Point(174, 326);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(94, 23);
+            this.openButton.TabIndex = 15;
+            this.openButton.Text = "Save Folder";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(849, 371);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(106, 17);
+            this.radioButton1.TabIndex = 17;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Client Commands";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(849, 392);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(104, 17);
+            this.radioButton2.TabIndex = 18;
+            this.radioButton2.Text = "Admin Command";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(849, 414);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(103, 17);
+            this.radioButton3.TabIndex = 19;
+            this.radioButton3.Text = "Game Command";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // helpbutton
+            // 
+            this.helpbutton.Location = new System.Drawing.Point(862, 437);
+            this.helpbutton.Name = "helpbutton";
+            this.helpbutton.Size = new System.Drawing.Size(75, 38);
+            this.helpbutton.TabIndex = 20;
+            this.helpbutton.Text = "What are these?";
+            this.helpbutton.UseVisualStyleBackColor = true;
+            this.helpbutton.Click += new System.EventHandler(this.helpbutton_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Image = global::BindsManager.Properties.Resources.icong;
+            this.pictureBox2.Location = new System.Drawing.Point(208, 360);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(49, 48);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = global::BindsManager.Properties.Resources.joinDiscord;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 358);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(182, 47);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(870, 445);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 23;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(272, 333);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(137, 13);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "Made by Ecliptedkillz#6969";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(272, 358);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(127, 13);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Freddie#0123 Smells bad";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(949, 487);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.helpbutton);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.exportButton);
+            this.Controls.Add(this.importButton);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.commandTextBox);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.addNewBindButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.autoFillButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "Bind Manager";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button autoFillButton;
+		private System.Windows.Forms.Button saveButton;
+		private System.Windows.Forms.Button addNewBindButton;
+		private System.Windows.Forms.Button clearButton;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.TextBox commandTextBox;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button importButton;
+        private System.Windows.Forms.Button exportButton;
+        private System.Windows.Forms.Button openButton;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.RadioButton radioButton3;
+		private System.Windows.Forms.Button helpbutton;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ BindsManager/Main.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using static BindsManager.Classes.InputBox;
+
+namespace BindsManager
+{
+	public partial class Main : Form
+	{
+		public string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "SCP Secret Laboratory", "cmdbinding.txt");
+        public string import_folder_path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "BindsManager");
+
+        public Main()
+		{
+			InitializeComponent();
+		}
+
+		private void Main_Load(object sender, EventArgs e)
+		{
+			ToolTip keyToolTip = new ToolTip();
+			keyToolTip.ShowAlways = true;
+			keyToolTip.SetToolTip(comboBox1, "Select a Key to bind the command to.");
+
+			ToolTip commandTip = new ToolTip();
+			commandTip.ShowAlways = true;
+			commandTip.SetToolTip(commandTextBox, "Type the command you want to bind, e.g. kill.");
+
+			richTextBox1.Text = File.ReadAllText(path);
+
+			richTextBox1.SelectionStart = richTextBox1.Text.Length;
+			richTextBox1.Focus();
+
+			foreach (var key in UnityKeyCodes.Keycodes.Keys)
+			{
+				comboBox1.Items.Add(key);
+			}
+
+            Directory.CreateDirectory(import_folder_path); // Create (internally ignores if already exists.)
+		}
+
+		private void clearButton_Click(object sender, EventArgs e)
+		{
+			richTextBox1.Text = "";
+		}
+
+		private void autoFillButton_Click(object sender, EventArgs e)
+		{
+			richTextBox1.Text += "51:.activateability\n52:.changeability\n102:.zfe";
+		}
+
+		private void addNewBindButton_Click(object sender, EventArgs e)
+		{
+			if (radioButton1.Checked) // its a . command
+			{
+				richTextBox1.Text += $"{UnityKeyCodes.Keycodes.Where(x => x.Key == comboBox1.Text).FirstOrDefault().Value}:.{commandTextBox.Text}\n";
+			}
+			else if (radioButton2.Checked) // it is an admin command
+			{
+				richTextBox1.Text += $"{UnityKeyCodes.Keycodes.Where(x => x.Key == comboBox1.Text).FirstOrDefault().Value}:/{commandTextBox.Text}\n";
+			}
+			else if (radioButton3.Checked) // it is a game console command
+			{
+				richTextBox1.Text += $"{UnityKeyCodes.Keycodes.Where(x => x.Key == comboBox1.Text).FirstOrDefault().Value}:{commandTextBox.Text}\n";
+			}
+		}
+
+		private void pictureBox1_Click(object sender, EventArgs e)
+		{
+			System.Diagnostics.Process.Start("https://discord.gg/Ducks");
+		}
+
+		private void pictureBox2_Click(object sender, EventArgs e)
+		{
+			System.Diagnostics.Process.Start("https://discord.gg/Ducks");
+		}
+
+		private void saveButton_Click(object sender, EventArgs e)
+		{
+			using (StreamWriter outputFile = new StreamWriter(path))
+			{
+				outputFile.Write(richTextBox1.Text);
+				MessageBox.Show("Saved your binds!", "All good!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+		}
+
+        private void importButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                dialog.InitialDirectory = import_folder_path;
+                dialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                dialog.FilterIndex = 2;
+                dialog.RestoreDirectory = true;
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    var path = dialog.FileName;
+                    var file = dialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(path))
+                    {
+                        richTextBox1.Text = reader.ReadToEnd();
+                        MessageBox.Show("Remember to press the Save button!", "Opened preset!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+        }
+
+        private void exportButton_Click(object sender, EventArgs e)
+        {
+            /*
+             using (FolderBrowserDialog dialog = new FolderBrowserDialog())
+            {
+                dialog.SelectedPath = import_folder_path;
+                
+                if (dialog.ShowDialog() == DialogResult.OK) {
+                    var result = AskInput("What is the name of this bind?", "Kognity's Bind Manager");
+
+                    var path = Path.Combine(import_folder_path, $"{result}.txt");
+
+                    var file = File.Create(path);
+
+                    using (StreamWriter writer = new StreamWriter(file))
+                    {
+                        writer.Write(richTextBox1.Text);
+                        MessageBox.Show($"The file was successfully exported as {result}!", "Exported file!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            
+            */
+
+            // Readme!
+            //
+            // Use code above instead if want it to be possible to choose folder (delete code below if enable code above) | Tested, works perfectly but seems like an extra unnecessary struggle for the user..
+            //
+            
+            var result = AskInput("What do you want to name this bind preset?", "Bind Manager");
+
+            if (result == "")
+            {
+                MessageBox.Show("You must specify a preset name!", "Bind Manager Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var path = Path.Combine(import_folder_path, $"{result}.txt");
+
+            var file = File.Create(path);
+
+            using (StreamWriter writer = new StreamWriter(file))
+            {
+                writer.Write(richTextBox1.Text);
+                MessageBox.Show($"The preset was successfully saved under name '{result}'!", "Saved preset!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            };
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+		{
+			richTextBox2.Text = "";
+			foreach(var line in richTextBox1.Text.Split('\n'))
+			{
+				try
+				{
+					richTextBox2.Text += line.Replace(line.Split(':')[0], UnityKeyCodes.Keycodes.Where(x => x.Value.ToString() == line.Split(':')[0]).FirstOrDefault().Key) + Environment.NewLine;
+				}
+				catch
+				{
+                    return;
+				}
+			}
+		}
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            Process.Start(@import_folder_path);
+        }
+
+		private void helpbutton_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("There are 3 major types of commands in SCP:SL\n\nFirstly are Remote Admin Commands. They are used either in the text based RA or typed in the game console with a / prefix (\"/tut Kognity\" as an example).\n\nSecondly are Client Commands. They are typed exlusively in the game console and are prefixed with a . (\".ability1\" as an example)\n\nLastly are Game Console Commands. They are locally run commands and are not sent to the server and have no prefix (\"ar\", \"rc\", \"connect SOMEIPADDRESS\" are some notable examples)", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
